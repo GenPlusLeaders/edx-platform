1f7f74ec86442d3eca3dbabc9ebf355226a8c6ee
zainvrk <zain.virk@edly.io>
2022-08-29 12:54:36 +0500

    added api for unlocking class lessons

--- openedx/features/genplus_features/genplus/display_messages.py
+++ openedx/features/genplus_features/genplus/display_messages.py
@@ -13,7 +13,8 @@ class ErrorMessages:
     INTERNAL_SERVER = 'Some error occur on the server. The request can not be completed for now.'
     ACTION_VALIDATION_ERROR = 'Action can only be add or remove'
     CLASS_ALREADY_ADDED = '{class_name} is already added in your classes.'
-    LESSON_ALREADY_UNLOCKED = "You cannot locked the lesson which is already unlocked."
+    LESSON_ALREADY_UNLOCKED = 'You cannot lock the lesson which is already unlocked.'
+    LESSON_NOT_FOUND = 'Lesson not found.'
     STUDENT_POST_ENTRY_FAILED = 'Journal post could not be added.'
     TEACHER_FEEDBACK_ENTRY_FAILED = 'Your feedback could not be added to this journal.'
     STUDENT_POST_UPDATE_FAILED = 'Your journal post could not be updated.'
