94517062617d2a9a63b538636f46cd995b092ca4
qasim.gulzar <qasim.gulzar@arbisoft.com>
2024-02-12 16:48:41 +0500

    🐛GP-965 - course badge missing.

--- openedx/features/genplus_features/genplus_badges/signals.py
+++ openedx/features/genplus_features/genplus_badges/signals.py
@@ -1,24 +1,24 @@
-from django.dispatch import receiver
 from django.db.models.signals import post_save
-from openedx.core.djangoapps.signals.signals import COURSE_COMPLETED
-from openedx.features.genplus_features.genplus_learning.models import UnitCompletion
-from openedx.features.genplus_features.genplus.models import Activity
+from django.dispatch import receiver
+
+from completion_aggregator.models import Aggregator
 from openedx.features.genplus_features.genplus.constants import ActivityTypes
-from openedx.features.genplus_features.genplus_badges.events.completion import (
-    unit_badge_check,
-    program_badge_check
-)
+from openedx.features.genplus_features.genplus.models import Activity
+from openedx.features.genplus_features.genplus_badges.events.completion import program_badge_check, unit_badge_check
+
 from .models import BoosterBadgeAward
 
 
-@receiver(COURSE_COMPLETED, sender=UnitCompletion)
-def create_unit_badge(sender, user, course_key, **kwargs):
-    unit_badge_check(user, course_key)
+@receiver(post_save, sender=Aggregator)
+def create_unit_badge(sender, instance, **kwargs):
+    if instance.aggregation_name == 'course' and instance.percent == 1:
+        unit_badge_check(instance.user, instance.course_key)
 
 
-@receiver(COURSE_COMPLETED, sender=UnitCompletion)
-def create_program_badge(sender, user, course_key, **kwargs):
-    program_badge_check(user, course_key)
+@receiver(post_save, sender=Aggregator)
+def create_program_badge(sender, instance, **kwargs):
+    if instance.aggregation_name == 'course' and instance.percent == 1:
+        program_badge_check(instance.user, instance.course_key)
 
 
 # capture activity on badge award
