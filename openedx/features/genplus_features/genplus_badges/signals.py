bb754fd2f41442392d5fa2c51533a228d163b7e0
Shavaiz Hussain <52816086+shavaiz-hussain@users.noreply.github.com>
2024-02-16 11:48:24 +0500

    Revert "fix: Aggregator integration" (#384)

--- openedx/features/genplus_features/genplus_badges/signals.py
+++ openedx/features/genplus_features/genplus_badges/signals.py
@@ -1,33 +1,24 @@
-from django.db.models.signals import post_save
 from django.dispatch import receiver
-
-from openedx.features.genplus_features.genplus.constants import ActivityTypes
+from django.db.models.signals import post_save
+from openedx.core.djangoapps.signals.signals import COURSE_COMPLETED
+from openedx.features.genplus_features.genplus_learning.models import UnitCompletion
 from openedx.features.genplus_features.genplus.models import Activity
-from openedx.features.genplus_features.genplus_badges.events.completion import program_badge_check, unit_badge_check
-
+from openedx.features.genplus_features.genplus.constants import ActivityTypes
+from openedx.features.genplus_features.genplus_badges.events.completion import (
+    unit_badge_check,
+    program_badge_check
+)
 from .models import BoosterBadgeAward
-from django.contrib.auth.models import User
-from completion_aggregator.dispatch import AggregatorUpdate
 
 
-@receiver(AggregatorUpdate)
-def create_unit_badge(sender, aggregation_data, **kwargs):
-    course = next(
-        filter(lambda aggregator: aggregator['aggregation_name'] == 'course', aggregation_data), None
-    )
-    if course and course['percent'] == 1:
-        user = User.objects.get(pk=course['user'])
-        unit_badge_check(user, course['course_key'])
+@receiver(COURSE_COMPLETED, sender=UnitCompletion)
+def create_unit_badge(sender, user, course_key, **kwargs):
+    unit_badge_check(user, course_key)
 
 
-@receiver(AggregatorUpdate)
-def create_program_badge(sender, aggregation_data, **kwargs):
-    course = next(
-        filter(lambda aggregator: aggregator['aggregation_name'] == 'course', aggregation_data), None
-    )
-    if course and course['percent'] == 1:
-        user = User.objects.get(pk=course['user'])
-        program_badge_check(user, course['course_key'])
+@receiver(COURSE_COMPLETED, sender=UnitCompletion)
+def create_program_badge(sender, user, course_key, **kwargs):
+    program_badge_check(user, course_key)
 
 
 # capture activity on badge award
