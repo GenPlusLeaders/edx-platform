9d1aed3058d0228032343a811bc31794c3877906
Muhammad Qasim Gulzar <19186089+qasimgulzar@users.noreply.github.com>
2024-03-07 12:14:00 +0500

    GP-965 Potential RDS performance issues (#386)

--- openedx/features/genplus_features/genplus_badges/signals.py
+++ openedx/features/genplus_features/genplus_badges/signals.py
@@ -1,7 +1,10 @@
+import logging
+from completion.models import BlockCompletion
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from opaque_keys.edx.keys import CourseKey
+
 from openedx.core.djangoapps.signals.signals import COURSE_COMPLETED
-from openedx.features.genplus_features.genplus_learning.models import UnitCompletion
 from openedx.features.genplus_features.genplus.models import Activity
 from openedx.features.genplus_features.genplus.constants import ActivityTypes
 from openedx.features.genplus_features.genplus_badges.events.completion import (
@@ -10,16 +13,20 @@ from openedx.features.genplus_features.genplus_badges.events.completion import (
 )
 from .models import BoosterBadgeAward
 
+logger = logging.getLogger(__name__)
 
-@receiver(COURSE_COMPLETED, sender=UnitCompletion)
+
+@receiver(COURSE_COMPLETED, sender=BlockCompletion)
 def create_unit_badge(sender, user, course_key, **kwargs):
-    unit_badge_check(user, course_key)
+    logger.info(f'Received course completed signal {str(user)},{str(course_key)}')
+    if isinstance(course_key, str):
+        course_key = CourseKey.from_string(course_key)
 
-
-@receiver(COURSE_COMPLETED, sender=UnitCompletion)
-def create_program_badge(sender, user, course_key, **kwargs):
-    program_badge_check(user, course_key)
-
+    try:
+        unit_badge_check(user, course_key)
+        program_badge_check(user, course_key)
+    except Exception as ex:
+        logger.exception(f'Something went wrong {str(ex)}')
 
 # capture activity on badge award
 @receiver(post_save, sender=BoosterBadgeAward)
