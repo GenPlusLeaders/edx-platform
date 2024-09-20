bed7449f88849ccb8e3c67480314f61f574f6464
shavaiz.hussain <shavaiz.hussain@arbisoft.com>
2024-09-20 10:34:05 +0500

    account page settings and display messages

--- openedx/features/genplus_features/common/display_messages.py
+++ openedx/features/genplus_features/common/display_messages.py
@@ -26,7 +26,7 @@ class ErrorMessages:
     ACTION_VALIDATION_ERROR = 'Action can only be add or remove'
     CLASS_ALREADY_ADDED = '{class_name} is already added in your classes.'
     LESSON_ALREADY_UNLOCKED = 'You cannot lock the lesson which is already unlocked.'
-    STUDENT_POST_ENTRY_FAILED = 'Journal post could not be added.'
+    STUDENT_POST_ENTRY_FAILED = 'Journal post could not be added. Please change the content of entry and try again.'
     TEACHER_FEEDBACK_ENTRY_FAILED = 'Your feedback could not be added to this journal.'
     STUDENT_POST_UPDATE_FAILED = 'Your journal post could not be updated.'
     TEACHER_FEEDBACK_UPDATE_FAILED = 'Your feedback could not be updated.'
